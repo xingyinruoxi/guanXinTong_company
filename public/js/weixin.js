ff18be3dbff58857882ca8c8f925683b4c803d6a
zhoulin <you@example.com>
2018-07-02 15:38:57 +0800

    首页相关完工

--- public/js/weixin.js
+++ public/js/weixin.js
@@ -31,5 +31,25 @@ $(function () {
 
         })
     });
+
+    $(document).on("pageInit", "#page-enroll", function (e, id, page) {
+        $('.list-entry-info').on('click','.link-close',function () {
+            $(this).closest('li').hide();
+        })
+    });
+    // 报名成功
+    $(document).on("pageInit", "#page-enroll-success", function (e, id, page) {
+        /*=== Popup ===*/
+        var myPhotoBrowserPopup = $.photoBrowser({
+            photos : [
+                '//img.alicdn.com/tps/i3/TB1kt4wHVXXXXb_XVXX0HY8HXXX-1024-1024.jpeg',
+            ],
+            theme: 'dark',
+            type: 'popup'
+        });
+        $(page).on('click','.pb-popup',function () {
+            myPhotoBrowserPopup.open();
+        });
+    });
     $.init();
 });
